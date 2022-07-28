06bc918c3de8803e9f2d949860d025d545de8b35
mafra27 <michelledesouza10@hotmail.com>
2022-07-28 20:24:34 -0300

    subindo atualizacoes

--- projetoFinal2/src/app.js
+++ projetoFinal2/src/app.js
@@ -1,12 +1,15 @@
+require("dotenv-safe").config()
 const express = require("express")
 const app = express()
 const database = require("./database/mongooseConnect")
 
-const aulasRouter = require("./routes/aulasRoutes")
+//const aulasRouter = require("./routes/aulasRoutes")
 
-app.use("/aulas", aulasRouter)
+//app.use("/aulas", aulasRouter)
 
 database.connect()
+
+app.get("/", (req,res) => res.status (200).send ("Esta funcionando"))
 
 module.exports= app
 
